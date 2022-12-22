44e237a23b05d2ab5c99631f6d5eb097702c2570
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-22 15:14:32 +1300

    add a mock bar chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,66 @@
 <script>
+	import { Bar } from 'svelte-chartjs';
+	import { data } from './data';
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		LinearScale,
+		PointElement,
+		CategoryScale
+	} from 'chart.js';
+
+	ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale, PointElement, CategoryScale);
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<section>
+	<Bar
+		{data}
+		options={{
+			responsive: true,
+			plugins: {
+				title: {
+					display: true,
+					text: 'Custom Chart Title',
+					color: 'hsl(0, 0%, 80%)'
+				},
+				legend: {
+					labels: {
+						color: 'hsl(0, 0%, 80%)'
+					}
+				}
+			},
+			scales: {
+				y: {
+					grid: {
+						color: 'hsl(10, 0%, 25%)'
+					},
+					title: {
+						display: true,
+						text: 'Value',
+						color: 'hsl(0, 0%, 80%)'
+					},
+					ticks: {
+						color: 'hsl(0, 0%, 80%)'
+					}
+				},
+				x: {
+					grid: {
+						color: ''
+					},
+					title: {
+						display: true,
+						text: 'Month',
+						color: 'hsl(0, 0%, 80%)'
+					},
 
-<div class="mx-2 flex bg-slate-50 " />
+					ticks: {
+						color: 'hsl(0, 0%, 80%)'
+					}
+				}
+			}
+		}}
+	/>
+</section>
