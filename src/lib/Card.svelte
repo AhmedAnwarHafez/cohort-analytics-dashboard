a30ead2985cc1794b12316e66ba0e09f7beb633e
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-22 18:47:05 +1300

    invert colors

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <article
-	class="col-span-2 flex flex-col gap-3 rounded-lg border-2 border-slate-400 bg-slate-500 p-8  text-slate-900"
+	class="col-span-2 flex flex-col gap-3 rounded-lg border-2 border-slate-700 bg-slate-900 p-8 text-slate-400"
 >
 	<p class="text-center text-5xl">{value}</p>
 	<p class="w-full text-center text-3xl">{description}</p>
