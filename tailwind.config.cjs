a570a76f85531887b4cd0ed052e523d532ea6e5a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-22 18:33:58 +1300

    add a custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				display: ['Kalam', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
