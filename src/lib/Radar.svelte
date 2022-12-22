0cf9a727659ff49f75b50f6f4f0c6c5d04181800
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-22 17:22:33 +1300

    make a radar component

---
+++ src/lib/Radar.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { Radar } from 'svelte-chartjs';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		PointElement,
+		RadialLinearScale,
+		LineElement
+	} from 'chart.js';
+
+	ChartJS.register(Title, Tooltip, Legend, PointElement, RadialLinearScale, LineElement);
+
+	const data = {
+		labels: ['Pupparazzi', 'Dreamfest', 'React-to-web-api', 'Sweet-as-organics', 'jwt-auth'],
+		datasets: [
+			{
+				label: 'Alice',
+				backgroundColor: 'rgba(194, 116, 161, 0.5)',
+				borderColor: 'rgb(194, 116, 161)',
+				data: [65, 59, 90, 81, 56]
+			},
+			{
+				label: 'Bob',
+				backgroundColor: 'rgba(71, 225, 167, 0.5)',
+				borderColor: 'rgb(71, 225, 167)',
+				data: [28, 48, 40, 19, 44]
+			}
+		]
+	};
+</script>
+
+<Radar
+	{data}
+	options={{
+		responsive: true,
+		scales: {
+			r: {
+				grid: {
+					color: 'hsl(0, 0%, 28%)'
+				},
+				pointLabels: {
+					color: 'hsl(0, 0%, 80%)'
+				}
+			}
+		}
+	}}
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
 <script>
 	import Card from '$lib/Card.svelte';
+	import Radar from '$lib/Radar.svelte';
 	import SinceForked from '$lib/SinceForked.svelte';
 </script>
 
-<section class="grid grid-cols-4 justify-items-center gap-6">
-	<Card value={2.3} description={'Average days taken to start a challenge'} />
-	<Card value={2.3} description={'Average days taken to start a challenge'} />
+<section class="flex flex-col gap-10">
+	<section class="grid grid-cols-4 justify-items-center gap-6">
+		<Card value={2.3} description={'Average days taken to start a challenge'} />
+		<Card value={2.3} description={'Average days taken to start a challenge'} />
+	</section>
+
+	<article>
+		<h3 class="text-lg text-slate-300">Days passed since fork</h3>
+		<p class="ml-2 text-lg text-slate-400">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
+			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
+			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
+		</p>
+		<SinceForked />
+	</article>
+
+	<article>
+		<h3 class="text-lg text-slate-300">Days passed since fork per challenge</h3>
+		<p class="ml-2 text-lg text-slate-400">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
+			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
+			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
+		</p>
+		<Radar />
+	</article>
 </section>
-<SinceForked />
