1750b96b43deb96af7af8e3231e01b8750af0b19
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-22 16:38:19 +1300

    make a new card component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let value: number;
+	export let description: string;
+</script>
+
+<article
+	class="col-span-2 flex flex-col gap-3 rounded-lg border-2 border-slate-400 bg-slate-500 p-8  text-slate-900"
+>
+	<p class="text-center text-5xl">{value}</p>
+	<p class="w-full text-center text-3xl">{description}</p>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+	import Card from '$lib/Card.svelte';
 	import SinceForked from '$lib/SinceForked.svelte';
 </script>
 
+<section class="grid grid-cols-4 justify-items-center gap-6">
+	<Card value={2.3} description={'Average days taken to start a challenge'} />
+	<Card value={2.3} description={'Average days taken to start a challenge'} />
+</section>
 <SinceForked />
