684972095e0ba7b309bab6abf4189e1e0b664cb9
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-24 08:48:06 +1300

    add mock data response for repos

---
+++ src/lib/data/repos.ts
@@ -0,0 +1,544 @@
+export const rawReposResponse = {
+	data: {
+		organization: {
+			repositories: {
+				edges: [
+					{
+						node: {
+							name: 'Happy-Doggy',
+							createdAt: '2022-12-01T00:44:54Z'
+						}
+					},
+					{
+						node: {
+							name: 'silent-spring',
+							createdAt: '2022-11-30T22:36:12Z'
+						}
+					},
+					{
+						node: {
+							name: 'exo-exo',
+							createdAt: '2022-11-30T21:50:55Z'
+						}
+					},
+					{
+						node: {
+							name: 'Carmigo',
+							createdAt: '2022-11-30T21:47:42Z'
+						}
+					}
+					// {
+					// 	node: {
+					// 		name: 'boilerplate-final-project',
+					// 		createdAt: '2022-11-30T01:44:20Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'eda-repo-migrator',
+					// 		createdAt: '2022-11-25T02:54:00Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'gardenz',
+					// 		createdAt: '2022-11-22T18:13:02Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'jwt-auth',
+					// 		createdAt: '2022-11-18T23:53:27Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'snack-lords',
+					// 		createdAt: '2022-11-17T21:21:53Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'ispy',
+					// 		createdAt: '2022-11-17T03:06:22Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'sweet-as-self',
+					// 		createdAt: '2022-11-17T03:03:57Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Team-FADS',
+					// 		createdAt: '2022-11-17T02:10:35Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'boilerplate-redux',
+					// 		createdAt: '2022-11-16T19:40:28Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'babylonjs-boilerplate',
+					// 		createdAt: '2022-11-16T01:32:33Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'todo-full-stack',
+					// 		createdAt: '2022-11-15T17:56:50Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'sweet-as-organics-api',
+					// 		createdAt: '2022-11-15T17:51:14Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'sweet-as-beers',
+					// 		createdAt: '2022-11-14T21:53:09Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'sweet-as-snacks',
+					// 		createdAt: '2022-11-13T17:57:28Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'who-let-the-dogs-out',
+					// 		createdAt: '2022-11-10T20:46:26Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'A-Team',
+					// 		createdAt: '2022-11-10T19:17:32Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'The-Weeknd',
+					// 		createdAt: '2022-11-10T04:41:34Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'team-3-sfbr',
+					// 		createdAt: '2022-11-10T04:16:14Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'boilerplate-fullstack',
+					// 		createdAt: '2022-11-10T00:19:50Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'boilerplate-react-api',
+					// 		createdAt: '2022-11-08T04:07:41Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'consuming-external-apis',
+					// 		createdAt: '2022-11-07T21:43:25Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'react-to-web-api',
+					// 		createdAt: '2022-11-07T21:36:53Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'charlottes-web-log-api',
+					// 		createdAt: '2022-11-06T19:24:29Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'redux-minimal-with-useReducer',
+					// 		createdAt: '2022-11-04T20:44:41Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'my-friends-outfits',
+					// 		createdAt: '2022-11-03T09:18:58Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'TEAM-READ-ONLY',
+					// 		createdAt: '2022-11-03T04:22:33Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'CAmoneysignH',
+					// 		createdAt: '2022-11-03T03:21:39Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Team-Foodies',
+					// 		createdAt: '2022-11-03T02:53:12Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'github-student-pack',
+					// 		createdAt: '2022-11-02T19:52:30Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'worldwide-routing',
+					// 		createdAt: '2022-11-01T19:56:47Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'enspiraled',
+					// 		createdAt: '2022-10-31T18:58:18Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'memory',
+					// 		createdAt: '2022-10-31T18:57:39Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'broken-kaleidoscope',
+					// 		createdAt: '2022-10-31T18:56:51Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'charlottes-web-log',
+					// 		createdAt: '2022-10-30T18:53:01Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'react-paws-for-effect',
+					// 		createdAt: '2022-10-30T18:52:24Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Alizaa-Ara-CP02',
+					// 		createdAt: '2022-10-29T01:54:48Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'team-awesome',
+					// 		createdAt: '2022-10-27T22:27:29Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Team-SSAA',
+					// 		createdAt: '2022-10-27T19:26:02Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'team-cjrd',
+					// 		createdAt: '2022-10-27T05:05:00Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'team-kewl',
+					// 		createdAt: '2022-10-27T04:28:55Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'boilerplate-knex',
+					// 		createdAt: '2022-10-27T02:18:41Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'dreamfest',
+					// 		createdAt: '2022-10-25T00:22:55Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'database-diagram',
+					// 		createdAt: '2022-10-25T00:22:36Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'knex-joins-stories',
+					// 		createdAt: '2022-10-24T21:12:54Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'knex-todo-cli',
+					// 		createdAt: '2022-10-23T20:37:24Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'aliyah-CP02',
+					// 		createdAt: '2022-10-21T07:24:43Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'team-BADD',
+					// 		createdAt: '2022-10-20T21:45:55Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: '5Brains_friday_project',
+					// 		createdAt: '2022-10-20T21:23:04Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'lightning-talks',
+					// 		createdAt: '2022-10-20T19:05:00Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Team-Frechiella-s-Project',
+					// 		createdAt: '2022-10-20T04:10:05Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Team-1-week2-grp-project',
+					// 		createdAt: '2022-10-20T03:26:11Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'RSS-CPO2',
+					// 		createdAt: '2022-10-19T22:15:55Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'dokku-checklist',
+					// 		createdAt: '2022-10-19T21:51:15Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Frances-CP02',
+					// 		createdAt: '2022-10-19T02:13:34Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'pupparazzi',
+					// 		createdAt: '2022-10-18T20:16:32Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'anna-CP02',
+					// 		createdAt: '2022-10-18T04:37:48Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Billie-CP02',
+					// 		createdAt: '2022-10-18T04:14:59Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'shymala-CP02',
+					// 		createdAt: '2022-10-18T00:41:40Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'charanjot-cp02',
+					// 		createdAt: '2022-10-17T23:47:56Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'server-side-rendering',
+					// 		createdAt: '2022-10-17T20:00:01Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'ciara-CP02',
+					// 		createdAt: '2022-10-17T18:18:50Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Sadhna-CP02',
+					// 		createdAt: '2022-10-17T08:59:23Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'KrysCP02',
+					// 		createdAt: '2022-10-17T08:22:32Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'jay-CP02',
+					// 		createdAt: '2022-10-17T07:59:53Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'elvin-CP02',
+					// 		createdAt: '2022-10-17T07:26:42Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'daphne-CP02',
+					// 		createdAt: '2022-10-17T05:01:23Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'Richard-CP02',
+					// 		createdAt: '2022-10-17T00:00:26Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'express-server',
+					// 		createdAt: '2022-10-16T18:49:06Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'dwalshjones-CP02',
+					// 		createdAt: '2022-10-14T05:30:03Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'ascii-art-reader',
+					// 		createdAt: '2022-10-13T19:26:22Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'conways',
+					// 		createdAt: '2022-10-12T21:34:37Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'tdd-bowling-kata',
+					// 		createdAt: '2022-10-12T21:34:10Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'bowling-kata',
+					// 		createdAt: '2022-10-11T19:40:02Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-data-structures',
+					// 		createdAt: '2022-10-11T19:39:30Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-fizzbuzz',
+					// 		createdAt: '2022-10-10T21:06:57Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-number-patterns',
+					// 		createdAt: '2022-10-10T21:06:30Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'object-array-practice',
+					// 		createdAt: '2022-10-10T21:06:02Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'exercise-arrays',
+					// 		createdAt: '2022-10-10T21:05:25Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-strings-numbers-modules',
+					// 		createdAt: '2022-10-10T21:04:58Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-types-modules',
+					// 		createdAt: '2022-10-10T21:04:30Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'kata-objects-and-arrays',
+					// 		createdAt: '2022-10-09T18:14:58Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'two-truths-and-a-lie',
+					// 		createdAt: '2022-10-09T18:14:30Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'spork-schedule',
+					// 		createdAt: '2022-10-09T18:09:26Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'code-from-class',
+					// 		createdAt: '2022-10-07T20:54:13Z'
+					// 	}
+					// },
+					// {
+					// 	node: {
+					// 		name: 'computer-setup',
+					// 		createdAt: '2022-10-03T21:21:53Z'
+					// 	}
+					// }
+				]
+			}
+		}
+	}
+};
