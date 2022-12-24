16a6d53ecb338226764cc05433d5f09229278a4e
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-24 21:04:42 +1300

    extract paragraph into its own comonent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import SinceForked from '$lib/SinceForked.svelte';
 	import Table from '$lib/Table.svelte';
 	import Bubble from '$lib/Bubble.svelte';
+	import Paragraph from '$lib/Paragraph.svelte';
 </script>
 
 <section class="flex flex-col gap-10">
@@ -14,46 +15,54 @@
 	</section>
 	<hr class="border-1 block h-1 border-slate-700" />
 	<article>
-		<h3 class="text-lg text-slate-300">Days passed since fork per challenge in table format</h3>
-		<p class="indent-8 text-lg text-slate-400">
+		<Paragraph
+			title="Days passed since fork per challenge in table format"
+			paragraph={`
 			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
 			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
 			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
-		</p>
+		`}
+		/>
 		<figure class="mt-5">
 			<Bubble data={$page.data.githubAggregates} />
 		</figure>
 	</article>
 	<hr class="border-1 block h-1 border-slate-700" />
 	<article>
-		<h3 class="text-lg text-slate-300">Days passed since fork per challenge in table format</h3>
-		<p class="indent-8 text-lg text-slate-400">
+		<Paragraph
+			title="Days passed since fork per challenge in table format"
+			paragraph={`
 			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
 			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
 			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
-		</p>
+		`}
+		/>
 		<figure class="mt-5">
 			<Table />
 		</figure>
 	</article>
 	<hr class="border-1 block h-1 border-slate-700" />
 	<article>
-		<h3 class="text-lg text-slate-300">Days passed since fork</h3>
-		<p class="indent-8 text-lg text-slate-400">
+		<Paragraph
+			title="Days passed since fork per challenge in table format"
+			paragraph={`
 			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
 			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
 			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
-		</p>
+		`}
+		/>
 		<SinceForked />
 	</article>
 	<hr class="border-1 block h-1 border-slate-700" />
 	<article>
-		<h3 class="text-lg text-slate-300">Days passed since fork per challenge</h3>
-		<p class="indent-8 text-lg text-slate-400">
+		<Paragraph
+			title="Days passed since fork per challenge in table format"
+			paragraph={`
 			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga necessitatibus sit
 			tenetur corporis culpa dignissimos illum mollitia exercitationem labore quibusdam temporibus
 			cumque consequatur inventore eligendi magnam laudantium ducimus nisi.
-		</p>
+		`}
+		/>
 		<Radar />
 	</article>
 </section>
