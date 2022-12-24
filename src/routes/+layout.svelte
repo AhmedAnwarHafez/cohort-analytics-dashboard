ebc158b02954df5ffb7b5f1407d9e87452b4719a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-25 06:03:00 +1300

    add gradients to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,12 +27,14 @@
 	rel="stylesheet"
 	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0"
 />
-<nav class="flex flex-row items-center justify-between bg-slate-900">
+<nav
+	class="flex flex-row items-center justify-between border-b border-b-slate-500 bg-gradient-to-r from-slate-900 via-slate-700 to-slate-900"
+>
 	<h1 class="pl-4 text-center font-display text-3xl text-slate-400 lg:text-left">
 		Cohort Analytics Dashboard
 	</h1>
 	<!-- a nav bar with three links, Github, Trello and Login -->
-	<ul class="flex flex-row  justify-end gap-4 space-x-6 bg-slate-900 p-4">
+	<ul class="flex flex-row  justify-end gap-4 space-x-6  p-4">
 		<li>
 			<a href="#" class="flex flex-row items-center gap-2 text-slate-400 hover:text-slate-300">
 				<span class="material-symbols-outlined">home</span>
