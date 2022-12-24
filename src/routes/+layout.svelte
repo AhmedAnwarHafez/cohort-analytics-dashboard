8e0b47177838b946fbebde6740cffb81c5970d9f
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-24 17:14:39 +1300

    make a GET form to filter results

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,92 @@
 <script lang="ts">
 	import '../app.css';
+	import type { Repo } from './+layout.server';
+	import { page } from '$app/stores';
+	import { slide } from 'svelte/transition';
+
+	export let data: { students: Student[]; repos: Repo[] };
+	// read query params from the URL
 
 	type Student = {
 		id: string;
 		login: string;
 	};
-	export let data: { students: Student[] };
 
+	$: selectedRepos = $page.url.searchParams.getAll('repos');
+	let selectedCohort: string;
 	let cohorts = [
-		'Harakeke-2022',
-		'Kahikatea-2022',
-		'Matai-2022',
-		'Pohutukawa-2022',
-		'Horoeka-2022'
+		'harakeke-2022',
+		'kahikatea-2022',
+		'matai-2022',
+		'pohutukawa-2022',
+		'horoeka-2022'
 	];
 </script>
 
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0"
+/>
 <main class="m-auto flex">
-	<aside class="flex flex-none basis-1/6 flex-col items-center">
-		<h1 class="p-10 text-center font-display text-3xl text-slate-400">
-			Cohort Analytics Dashboard
-		</h1>
-		<select
-			name="cohorts"
-			id="cohorts"
-			class="mb-2 w-40 rounded-lg bg-slate-700 p-2 text-slate-300"
-		>
-			{#each cohorts as cohort}
-				<option value={cohort}>{cohort}</option>
-			{/each}
-		</select>
-		<ul class="text-lg text-slate-500">
-			{#each data.students as { login: name }}
-				<li class="py-1">{name}</li>
-			{/each}
-		</ul>
+	<aside class="ml-12 flex flex-none basis-1/6 flex-col items-start gap-4">
+		<h1 class="m-10 ml-0 font-display text-3xl text-slate-400">Cohort Analytics Dashboard</h1>
+
+		<form method="get">
+			<select
+				name="cohort"
+				id="cohort"
+				class="mb-2 w-40 rounded-lg bg-slate-700 p-2 text-slate-300"
+				required
+				bind:value={selectedCohort}
+			>
+				<option value="">--COHORTS--</option>
+				{#each cohorts as cohort}
+					<option value={cohort}>{cohort}</option>
+				{/each}
+			</select>
+			<ul class="flex flex-col gap-4">
+				{#each data.repos as { name }}
+					<li>
+						<label for={name} class="flex items-center gap-1 text-2xl text-slate-400">
+							<!-- <input type="hidden" name="id" value={name} /> -->
+							{#if selectedRepos.includes(name)}
+								<input
+									type="checkbox"
+									name="repos"
+									id={name}
+									class="h-6 w-6  bg-slate-100 text-green-500 focus:ring-slate-500"
+									value={name}
+									checked={true}
+								/>
+							{:else}
+								<input
+									type="checkbox"
+									name="repos"
+									id={name}
+									class="h-6 w-6  bg-slate-100 text-green-500 focus:ring-slate-500"
+									value={name}
+								/>
+							{/if}
+							{name}
+						</label>
+					</li>
+				{/each}
+			</ul>
+			<button
+				class="align-center mt-4 flex items-center gap-2 rounded bg-slate-600 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-slate-700 hover:shadow-lg focus:bg-slate-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-800 active:shadow-lg"
+			>
+				<span class="material-symbols-outlined"> tune </span>
+				Filter
+			</button>
+		</form>
 	</aside>
 	<section class="col-span-11 mt-5 mr-5 w-1/6 grow">
-		<slot />
+		{#if data.students.length > 0}
+			<article in:slide>
+				<slot />
+			</article>
+		{:else}
+			<p class="text-center text-6xl text-slate-200">Use the filter</p>
+		{/if}
 	</section>
 </main>
