e688dadc4ee0a6a3b481f8d4fa1f74397d8e0baf
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-26 06:20:59 +1300

    add a story for card

---
+++ src/lib/Card.story.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	import { Canvas, Frame } from '@leveluptuts/bookit';
+	import Card from './Card.svelte';
+
+	export const parent = 'components';
+	export const title = 'parent';
+</script>
+
+<Canvas>
+	<Frame title="Basic Card">
+		<Card description="some description" value={99} />
+	</Frame>
+</Canvas>
