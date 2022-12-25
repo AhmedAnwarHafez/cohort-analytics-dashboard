0680db11b1d21826cf6326d083f514ca9835d060
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-25 17:24:41 +1300

    remove mock cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,6 @@
 <section class="flex flex-col gap-10">
 	<section class="flex flex-wrap items-stretch  justify-center gap-4">
 		<Card value={orderedStudents.length} description={'Total Students'} />
-		<Card value={2.3} description={'Average days taken to start a challenge'} />
-		<Card value={2.3} description={'Average days taken to start a challenge'} />
 	</section>
 	<article>
 		<ol class="flex list-decimal flex-col items-center justify-center">
