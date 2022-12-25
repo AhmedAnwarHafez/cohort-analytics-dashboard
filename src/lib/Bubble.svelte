b10523f37f866e676a01c538c14b1bdc51b28195
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-25 15:45:35 +1300

    render chart for every challenge

--- src/lib/Bubble.svelte
+++ src/lib/Bubble.svelte
@@ -4,66 +4,69 @@
 
 	import { Chart as ChartJS, Title, Tooltip, Legend, PointElement, LinearScale } from 'chart.js';
 	import type { StudentGithubAggregate } from 'src/routes/+layout.server';
+	import { groupBy } from 'lodash';
 
 	ChartJS.register(Title, Tooltip, Legend, PointElement, LinearScale);
 
 	export let data: StudentGithubAggregate[];
+	$: groupedByRepo = groupBy(data, 'repo');
 
-	const datasets = data.map((student) => ({
-		label: student.githubLogin,
-		// get a random color for each student
-		backgroundColor: `hsl(${hash(student.githubLogin) % 360}, 35%, 50%)`,
-		hoverBackgroundColor: `hsl(${hash(student.githubLogin) % 360}, 35%, 50%)`,
-		data: [
-			{
-				x: student.daysSinceForked,
-				y: student.daysSpentOnChallenge,
-				// r is the size of the bubble, 5 is the minimum size
-				r: 5 + student.totalCount
-			}
-		]
-	}));
+	function getByRepo(repo: string) {
+		return data
+			.filter((row) => row.repo === repo)
+			.map((student) => ({
+				label: student.githubLogin,
+				// get a random color for each student
+				backgroundColor: `hsl(${hash(student.githubLogin) % 360}, 35%, 50%)`,
+				hoverBackgroundColor: `hsl(${hash(student.githubLogin) % 360}, 35%, 50%)`,
+				data: [
+					{
+						x: student.daysSinceForked,
+						y: student.daysSpentOnChallenge,
+						// r is the size of the bubble, 5 is the minimum size
+						r: 5 //+ student.totalCount
+					}
+				]
+			}));
+	}
 </script>
 
-<Bubble
-	data={{
-		labels: 'Bubble',
-		datasets
-	}}
-	options={{
-		responsive: true,
-		plugins: {
-			title: {
-				display: true,
-				text: 'Days to complete vs days since forked',
-				color: 'hsl(0, 0%, 80%)',
-				font: {
-					size: 15
+{#each Object.keys(groupedByRepo) as repo}
+	<figure class="mt-5">
+		<figcaption class="text-center text-4xl text-slate-400">{repo}</figcaption>
+		<Bubble
+			data={{
+				labels: 'Bubble',
+				datasets: getByRepo(repo)
+			}}
+			options={{
+				responsive: true,
+				plugins: {
+					legend: {
+						labels: {
+							color: 'hsl(0, 0%, 80%)'
+						}
+					}
+				},
+				scales: {
+					x: {
+						beginAtZero: true,
+						title: {
+							display: true,
+							text: 'Days spent to complete challenge',
+							color: 'hsl(0, 0%, 80%)'
+						}
+					},
+					y: {
+						beginAtZero: true,
+						title: {
+							display: true,
+							text: 'Days since challenge forked',
+							color: 'hsl(0, 0%, 80%)'
+						}
+					}
 				}
-			},
-			legend: {
-				labels: {
-					color: 'hsl(0, 0%, 80%)'
-				}
-			}
-		},
-		scales: {
-			x: {
-				beginAtZero: true,
-				title: {
-					display: true,
-					text: 'Days spent to complete challenge',
-					color: 'hsl(0, 0%, 80%)'
-				}
-			},
-			y: {
-				beginAtZero: true,
-				title: {
-					display: true,
-					text: 'Days since challenge forked',
-					color: 'hsl(0, 0%, 80%)'
-				}
-			}
-		}
-	}}
-/>
+			}}
+		/>
+	</figure>
+{/each}
