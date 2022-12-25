fbe7d9a080063c570ec17f131794a931f17ad61a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2022-12-26 06:20:48 +1300

    setup and configure bookit

---
+++ src/routes/(book)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Book } from '@leveluptuts/bookit';
+	import '../../../src/app.css';
+</script>
+
+<Book>
+	<slot />
+</Book>
---
+++ src/routes/(book)/book/[parent]-[title]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Bookit } from '@leveluptuts/bookit';
+	type Data = {
+		props: {
+			params: {
+				parent: string;
+				title: string;
+			};
+		};
+	};
+	export let data: Data;
+</script>
+
+<Bookit params={data.props.params} />
