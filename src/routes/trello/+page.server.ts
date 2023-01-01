fe3b6bf7acc5826c1aaac1743158f3356313f533
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-01-01 14:28:08 +1300

    calculate the number of bounces from trello api

--- src/routes/trello/+page.server.ts
+++ src/routes/trello/+page.server.ts
@@ -1,4 +1,6 @@
 import type { Actions, PageServerLoad } from './$types';
+// import fetchAll from './trello';
+import { boards } from './boards';
 
 export type Data = Awaited<ReturnType<typeof load>>;
 
@@ -6,11 +8,34 @@ export const load = (async ({ cookies }) => {
 	const data = cookies.get('boards');
 
 	if (data) {
-		const boards = JSON.parse(data);
-		console.log(boards);
+		const bounces = boards
+			// put the cards in the board object
+			.map((board) => ({
+				...board,
+				cards: board.lists.flatMap((list) => list.cards)
+			}))
+			.map((board) => ({
+				name: board.name,
+				bounces: board.cards
+					// filter out cards that don't have actions
+					.filter((card) => card.actions.length > 0)
+					// filter out cards that don't have a CP or WD in the name
+					.filter((card) => card.name.match(/cp/i) || card.name.match(/WD/i))
+					// calculate the number of moves from For Review to Needs Action
+					.reduce(
+						(acc, card) =>
+							card.actions.some(
+								(action) =>
+									action.after.match(/Needs Action/i) && action.before.match(/For Review/i)
+							)
+								? acc + 1
+								: acc,
+						0
+					)
+			}));
 
 		return {
-			boards
+			boards: bounces
 		};
 	}
 
