e0714ff4f5791196aac471ea8d9437b8823cbb08
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-01-01 14:45:08 +1300

    improve styling

--- src/routes/trello/+page.svelte
+++ src/routes/trello/+page.svelte
@@ -5,22 +5,28 @@
 	export let data: Data;
 </script>
 
-<main class="container flex flex-col">
-	<h2>Trello</h2>
-
-	<form method="post" use:enhance>
-		<label for="board">
-			Board name
-			<input type="text" name="board" />
+<main class="container mt-10 flex flex-col gap-8">
+	<form method="post" use:enhance class="mx-auto">
+		<label for="board" class="flex flex-col">
+			<span class="text-slate-400">Board Name</span>
+			<input type="text" name="board" class="text-4xl" />
 		</label>
 	</form>
 
-	{#each data.boards as { name, bounces }}
-		<div class="board">
-			<h3>{name}</h3>
-			<h3>{bounces}</h3>
-		</div>
-	{:else}
-		<p>No boards found</p>
-	{/each}
+	<table class="mx-auto w-4/5 table-auto text-3xl text-slate-300">
+		<thead>
+			<tr>
+				<th>Bounces</th>
+				<th>Name</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.boards as { name, bounces }}
+				<tr>
+					<td class="border border-slate-400 p-4">{bounces}</td>
+					<td class="border border-slate-400 p-4">{name}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </main>
