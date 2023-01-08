ab194a463c3b8c99c1c23fe3d308ec1c5b9fb7cc
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-01-08 17:46:34 +1300

    fix adding and removing searchparams

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -13,13 +13,22 @@
 		// this function is called when the select element or the checkbox is changed
 		// since the select element is a single value, we can just set the value
 		// but the checkbox is a multi-value, so we need to add/remove the value
+		// we need to consider that if the checkbox is unchecked, we need to remove the value
+		// and keep the other values
 		// we can use the `URLSearchParams` API to make this easier
 		const params = new URLSearchParams($page.url.search);
 		if (target.type === 'checkbox') {
 			if (target.checked) {
 				params.append(name, value);
 			} else {
-				params.delete(name);
+				// we need to remove the value from the URL
+				// we can't use `params.delete(name)` because it will remove all the values
+				// so we need to get all the values, remove the value we want to remove
+				// and then add the new values
+				const values = params.getAll(name); // get all the values
+				params.getAll(name).forEach(() => params.delete(name)); // remove all the values, we will add them back later
+				const newValues = values.filter((v) => v !== value); // remove the value we want to remove
+				newValues.forEach((v) => params.append(name, v)); // finally, add the new values
 			}
 		} else {
 			params.set(name, value);
