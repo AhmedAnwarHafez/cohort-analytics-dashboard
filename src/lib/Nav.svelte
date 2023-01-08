7f7bfd412d5c7c9c5cedaa81e6ed62b7f91cefa4
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-01-08 14:40:08 +1300

    display logo

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,7 +6,14 @@
 <nav
 	class="flex flex-row items-center justify-between border-b border-b-slate-500 bg-slate-900 p-4"
 >
-	<h1 class="pl-4 text-center text-3xl text-slate-400 lg:text-left">Cohort Analytics Dashboard</h1>
+	<section class="flex">
+		<picture>
+			<img src="/logo.png" alt="Logo of Dev Academy" class="h-10 w-10" />
+		</picture>
+		<h1 class="pl-4 text-center text-3xl text-slate-400 lg:text-left">
+			Cohort Analytics Dashboard
+		</h1>
+	</section>
 	<!-- a nav bar with three links, Github, Trello and Login -->
 	<ul class="flex flex-row  justify-end gap-4 space-x-6  p-4">
 		<li>
